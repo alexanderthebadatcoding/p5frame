16bbea5a290f4ceff8340a7d77679e704a9d7bc7
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2024-02-16 12:37:16 -0600

    Update poster.ts

--- src/frames/poster.ts
+++ src/frames/poster.ts
@@ -2,19 +2,20 @@ import { FrameActionDataParsed } from "frames.js";
 const html = String.raw;
 
 export default {
-    name: 'poster',
-    logic: (message: FrameActionDataParsed) => {
-        if (message.buttonIndex == 1) {
-            return `count`
-        }
-    },
-    content: () => html`
-        <frame-image src="/images/poster-animated.gif" />
-        <frame-button>
-           Art
-        </frame-button>
-        <frame-button action="link" target="https://github.com/depatchedmode/simplest-frame">
-            {😺} View on Github
-        </frame-button>
-    `
+  name: "poster",
+  logic: (message: FrameActionDataParsed) => {
+    if (message.buttonIndex == 1) {
+      return `count`;
+    }
+  },
+  content: () => html`
+    <frame-image src="/images/poster-animated.gif" />
+    <frame-button> test </frame-button>
+    <frame-button
+      action="link"
+      target="https://github.com/depatchedmode/simplest-frame"
+    >
+      {😺} View on Github
+    </frame-button>
+  `,
 };

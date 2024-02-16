964196304c9be3565b06886effec8a07d7c3079a
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2024-02-15 22:37:38 -0600

    added p5 and jsdom

---
+++ src/frames/art.ts
@@ -0,0 +1,49 @@
+// import jsdom from "jsdom";
+import { JSDOM } from "jsdom";
+const html = String.raw;
+
+// Create a virtual DOM environment
+const window = new JSDOM("<!DOCTYPE html>").window;
+global.window = window;
+global.screen = window.screen;
+global.navigator = window.navigator;
+// global.Image = window.Image;
+// global.Node = window.Node;
+
+// Import p5.js
+import P5 from "p5";
+// import "p5/lib/addons/p5.dom";
+
+// Your p5.js sketch
+const sketch = (p) => {
+  p.setup = () => {
+    p.createCanvas(400, 200);
+    p.background(220);
+    p.noCanvas();
+  };
+
+  p.draw = () => {
+    p.fill(255, 0, 0);
+    p.ellipse(50, 100, 50, 50);
+    p.canvas.toDataURL();
+    console.log(data);
+  };
+};
+
+// Create a new p5 instance
+new P5(sketch);
+
+// Export the canvasDataURL in the desired format
+
+export default {
+  name: "poster",
+  logic: () => null,
+  content: () => html`
+    <frame-image src="${data}" />
+    <frame-button> 🔳 Try Demo </frame-button>
+    <frame-button action="link" target="${process.env.STOLEN_REDIRECT_URL}">
+      👩‍🎤 View original cast
+    </frame-button>
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.js"></script>
+  `,
+};

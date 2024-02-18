edb05d3aac85b0a4f8e8f4eec232d95b145e7b18
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2024-02-18 10:33:33 -0600

    add p5 sketch

--- api/sketch.ts
+++ api/sketch.ts
@@ -1,7 +1,6 @@
-import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 export default async (req, context) => {
-
   try {
     const options = {
       headless: true,
@@ -13,7 +12,8 @@ export default async (req, context) => {
 
     // Adjust based on the environment
     if (process.env.IS_LOCAL) {
-      options.executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+      options.executablePath =
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
     } else {
       options.executablePath = await chromium.executablePath();
       options.args = chromium.args;
@@ -42,12 +42,177 @@ export default async (req, context) => {
         </head>
         <body>
           <script>
-            function setup() {
-              createCanvas(1200, 630);
-              background(Math.random()*255,Math.random()*255,Math.random()*255);
-              fill(Math.random()*255, Math.random()*255, Math.random()*255);
-              ellipse(Math.random()*1200, Math.random()*630, Math.random()*630, Math.random()*630);
+            let tiles = [];
+            let cols;
+            let rows;
+            let size;
+            // let colors;
+            let vexArc;
+            let numShapes;
+            let img;
+            let colors = [];
+            let numColors = 5;
+            let baseColor;
+            let complementaryColor;
+
+            function preload() {
+              // Load your image
+              img = loadImage(
+                "https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_144/https%3A%2F%2Fi.imgur.com%2FcwUW3lt.jpg"
+              );
             }
+
+            class Tile {
+              constructor(x, y, type, c) {
+                this.x = x;
+                this.y = y;
+                this.type = type;
+                this.c = c;
+              }
+
+              display() {
+                push();
+                translate(this.x, this.y);
+                if (vexArc == 1) {
+                  stroke(this.c);
+                  noFill();
+                  strokeWeight(5);
+                  if (this.type == 0) {
+                    arc(0, 0, size, size, 0, 90);
+                    arc(size, size, size, size, 180, 270);
+                    arc(size, size, size, size, 180, 270);
+                  } else {
+                    arc(size, 0, size, size, 90, 180);
+                    arc(0, size, size, size, 270, 360);
+                  }
+                } else {
+                  fill(this.c);
+                  noStroke();
+                  beginShape();
+                  if (this.type == 0) {
+                    // Shape 1
+                    vertex(size, 0);
+                    vertex(size, size);
+                    vertex(0, size);
+                  } else if (this.type == 1) {
+                    // Shape 2
+                    vertex(size, 0);
+                    vertex(0, 0);
+                    vertex(0, size);
+                  } else if (this.type == 2) {
+                    // Shape 3
+                    vertex(size, size);
+                    vertex(0, 0);
+                    vertex(0, size);
+                  } else {
+                    // Shape 4
+                    vertex(size, size);
+                    vertex(0, 0);
+                    vertex(size, 0);
+                  }
+                  endShape();
+                }
+
+                pop();
+              }
+            }
+
+            function setup() {
+              // randomSeed(1738);
+              vexArc = floor(random(2));
+              if (vexArc == 1) {
+                numShapes = 2;
+                angleMode(DEGREES);
+              } else {
+                numShapes = 4;
+              }
+              // print(vexArc, numShapes);
+
+              size = floor(random(30, 60));
+              createCanvas(1200, 630);
+              // Get the colors from the image
+              getColorsFromImage(img);
+              baseColor = colors[floor(random(5))];
+              complementaryColor = getComplementaryColor(baseColor);
+              // colors = ["#0a1045", "#00c2d1", "#f9e900", "#f6af65", "#ed33b9"];
+
+              cols = width / size;
+              rows = height / size;
+              for (let i = 0; i < cols; i++) {
+                tiles[i] = [];
+                for (let j = 0; j < rows; j++) {
+                  tiles[i][j] = new Tile(
+                    i * size,
+                    j * size,
+                    floor(random(numShapes)),
+                    colors[floor(random(5))]
+                  );
+                }
+              }
+            }
+
+            function draw() {
+              background(complementaryColor);
+              for (let i = 0; i < cols; i++) {
+                for (let j = 0; j < rows; j++) {
+                  tiles[i][j].display();
+                }
+              }
+              noLoop();
+            }
+
+            function getColorsFromImage(img) {
+              img.loadPixels();
+              colors = [];
+
+              while (colors.length < numColors) {
+                let x = int(random(img.width));
+                let y = int(random(img.height));
+
+                // Get the color at the random position
+                let index = (y * img.width + x) * 4;
+                let r = img.pixels[index];
+                let g = img.pixels[index + 1];
+                let b = img.pixels[index + 2];
+
+                // Convert RGB to hex
+                let colorHex = rgbToHex(r, g, b);
+
+                // Check for duplicate color
+                if (!colors.includes(colorHex)) {
+                  colors.push(colorHex);
+                  // print(colorHex);
+                }
+              }
+            }
+
+            function rgbToHex(r, g, b) {
+              return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+            }
+
+            function componentToHex(c) {
+              let hex = c.toString(16);
+              return hex.length == 1 ? "0" + hex : hex;
+            }
+
+            function getComplementaryColor(baseColor) {
+              let r = red(baseColor);
+              let g = green(baseColor);
+              let b = blue(baseColor);
+
+              // Calculate complementary color
+              let compR = 255 - r;
+              let compG = 255 - g;
+              let compB = 255 - b;
+              // Adjust brightness to make it darker (you can modify this factor)
+              let darkerFactor = 0.47; // Adjust as needed
+              compR *= darkerFactor;
+              compG *= darkerFactor;
+              compB *= darkerFactor;
+
+              return color(compR, compG, compB);
+            }
+
             window.onload = function() {
               new p5();
             };
@@ -58,21 +223,23 @@ export default async (req, context) => {
 
     await page.setContent(sketch);
     const renderDelay = 250; // in ms, how long you want to wait before capturing the screenshot
-    await new Promise(resolve => setTimeout(resolve, renderDelay)); // Wait a bit for the sketch to render
+    await new Promise((resolve) => setTimeout(resolve, renderDelay)); // Wait a bit for the sketch to render
 
-    const imageBuffer = await page.screenshot({ type: 'png' });
+    const imageBuffer = await page.screenshot({ type: "png" });
     await browser.close();
 
     return new Response(imageBuffer, {
       status: 200,
-      headers: { 'Content-Type': 'image/png' },
+      headers: { "Content-Type": "image/png" },
     });
   } catch (error) {
-    console.error('Error generating sketch:', error);
-    return new Response('Server Error: Cannot generate sketch', { status: 500 });
+    console.error("Error generating sketch:", error);
+    return new Response("Server Error: Cannot generate sketch", {
+      status: 500,
+    });
   }
-}
+};
 
 export const config = {
-  path: "/sketch"
+  path: "/sketch",
 };
--- src/frames/credits.ts
+++ src/frames/credits.ts
@@ -2,19 +2,20 @@ import { FrameActionDataParsed } from "frames.js";
 const html = String.raw;
 
 export default {
-    name: 'credits',
-    logic: (message: FrameActionDataParsed) => {
-        if (message.buttonIndex == 1) {
-            return `count`
-        }
-    },
-    content: () => html`
-        <frame-image src="/images/credits.png" />
-        <frame-button>
-            ⬅️ Back
-        </frame-button>
-        <frame-button action="link" target="https://github.com/depatchedmode/simplest-frame">
-            {😺} View on Github
-        </frame-button>
-    `
+  name: "credits",
+  logic: (message: FrameActionDataParsed) => {
+    if (message.buttonIndex == 1) {
+      return `count`;
+    }
+  },
+  content: () => html`
+    <frame-image src="/images/credits.png" />
+    <frame-button> ⬅️ Back </frame-button>
+    <frame-button
+      action="link"
+      target="https://www.youtube.com/watch?v=9DU5WiNdu74"
+    >
+      View Youtube Tutorial
+    </frame-button>
+  `,
 };
